60e8ca19a288b0c7e1bb07da35985361f5f95c76
HernanCDEE79 <hisepro.hlopez@gmail.com>
2026-04-29 16:05:30 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'palletpro-cdic-v2';
+const CACHE_VERSION = 'palletpro-cdic-v3';
 const ASSETS = ['./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_VERSION).then(c=>c.addAll(ASSETS).catch(()=>{})).then(()=>self.skipWaiting()));
